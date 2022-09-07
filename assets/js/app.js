4d7100697f489003a2bde50a105d04d55d18e38d
fender01ast <rus.k1996@gmail.com>
2022-09-08 01:34:43 +0600

    ver 1.1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,19 @@
-function zoom(e){
-    var zoomer = e.currentTarget;
-    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
-    x = offsetX/zoomer.offsetWidth*100
-    y = offsetY/zoomer.offsetHeight*100
-    zoomer.style.backgroundPosition = x + '% ' + y + '%';
-    zoomer.style.backgroundImage = "url('assets/images/compressed/завод-обрез2.jpg')";
+if ( $( window ).width() >= 992 ) {
+    function zoom(e){
+        var zoomer = e.currentTarget;
+        e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+        e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+        x = offsetX/zoomer.offsetWidth*100
+        y = offsetY/zoomer.offsetHeight*100
+        zoomer.style.backgroundPosition = x + '% ' + y + '%';
+        zoomer.style.backgroundImage = "url('assets/images/compressed/завод-обрез2.jpg')";
+    }
+    function zoomout(e){
+        var zoomer = e.currentTarget;
+        zoomer.style.backgroundImage = "none";
+    }
 }
-function zoomout(e){
-    var zoomer = e.currentTarget;
-    zoomer.style.backgroundImage = "none";
-}
+
 
 document.addEventListener("DOMContentLoaded", function(){
 
